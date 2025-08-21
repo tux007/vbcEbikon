2cc96895946d54ecc21a58458bbcfc00e41e7d0b
Thomas Steimann <thomas@steimann.ch>
2025-08-21 18:54:04 +0200

    Text (Funktion / Name) unterhalb Bilder hinzugefügt

--- js/vorstand.js
+++ js/vorstand.js
@@ -1,3 +1,29 @@
+const vorstandData = [
+  { name: "Thomas Steimann", funktion: "Präsident" },
+  { name: "Selina Poletti", funktion: "Vizepräsidentin / Anlässe" },
+  { name: "Samuel Tartarotti", funktion: "Hallenvolleyball" },
+  { name: "Tanja Burri", funktion: "Meisterschaft" },
+  { name: "Nadin Andergassen", funktion: "Beachvolleyball / Material" },
+  { name: "Sara Mattmann", funktion: "Presse / Sponsoring" },
+  { name: "Marc Beissl", funktion: "Finanzen" },
+  { name: "Aline Petermann", funktion: "Administration" }
+];
+
+document.querySelectorAll('.vorstand-img-block').forEach((block, i) => {
+  const img = block.querySelector('img');
+  let info = document.createElement('div');
+  info.className = 'vorstand-hover-info-text';
+  block.appendChild(info);
+
+  img.addEventListener('mouseenter', function () {
+    info.innerHTML = `<strong>${vorstandData[i].funktion}</strong><br>${vorstandData[i].name}`;
+    info.style.display = 'block';
+  });
+  img.addEventListener('mouseleave', function () {
+    info.style.display = 'none';
+    info.innerHTML = '';
+  });
+});
 var $g = document.querySelector(".gallery");
 var $h = document.querySelector(".horizontal");
 
