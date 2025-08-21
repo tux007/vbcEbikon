7cf3815b4ae0576c1a22a55c43711678b33a2b14
Thomas Steimann <thomas@steimann.ch>
2025-08-21 18:16:42 +0200

    Vorstandseite Draft

---
+++ js/vorstand.js
@@ -0,0 +1,15 @@
+var $g = document.querySelector(".gallery");
+var $h = document.querySelector(".horizontal");
+
+var translate = 0;
+const halfViewport = window.innerWidth / 2;
+window.addEventListener("wheel", (e) => {
+  event.preventDefault();
+  const finalPosition = translate + -e.deltaY;
+  if (finalPosition < halfViewport && finalPosition > (-6 * halfViewport)) {
+    translate += -e.deltaY;
+
+    $h.style.transform = `translate(${translate}px)`;
+  }
+}, { passive: false });
+
